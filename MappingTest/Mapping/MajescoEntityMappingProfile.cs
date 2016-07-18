2b3425f2d8ccdd09bc116ea13438b8f6fddfc22f
Tim Boyce <tboyce@gmail.com>
2016-07-18 17:30:02 -0400

    add new() type constraint

--- MappingTest/Mapping/MajescoEntityMappingProfile.cs
+++ MappingTest/Mapping/MajescoEntityMappingProfile.cs
@@ -1,10 +1,10 @@
-using System;
 using AutoMapper;
 using MappingTest.MmgModels;
 
 namespace MappingTest.Mapping
 {
     public class MajescoEntityMappingProfile<TDest> : Profile
+        where TDest : new()
     {
         public MajescoEntityMappingProfile()
         {
@@ -17,7 +17,7 @@ namespace MappingTest.Mapping
         {
             public TDest Convert(Person source, TDest destination, ResolutionContext context)
             {
-                if (destination == null) destination = Activator.CreateInstance<TDest>();
+                if (destination == null) destination = new TDest();
 
                 var destination1 = destination as dynamic;
 
